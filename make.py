c5e84514d1ce6966cc5f20540edc607025b85f71
jinwoo.jo <wlsdnen@gmail.com>
2017-05-24 21:57:17 +0900

    simple model

--- make.py
+++ make.py
@@ -5,10 +5,10 @@ wf2 = open("s2.txt", "a")
 wf3 = open("s3.txt", "a")
 
 
-def getRandList1(l):	
+def getRandList1(l):
 	return [str(random.randrange(1,20)) for i in range(l)]
 
-def getRandList2(l):	
+def getRandList2(l):
 	return [str(random.randrange(120,200)) for i in range(l)]
 
 
@@ -16,5 +16,3 @@ for i in range(1000):
 	wf1.write(",".join(getRandList1(10)) + "\n")
 	wf2.write(",".join(getRandList2(10)) + "\n")
 	wf3.write("%s\n" %(0))
-
-		
---
+++ test.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+import random
+
+tf.set_random_seed(777)  # reproducibility
+
+train1 = np.loadtxt('DuplicateDetectionModel/s1.txt', delimiter=',', dtype=np.float32)
+train2 = np.loadtxt('DuplicateDetectionModel/s2.txt', delimiter=',', dtype=np.float32)
+target = np.loadtxt('DuplicateDetectionModel/s3.txt', delimiter=',', dtype=np.float32)
+target = np.reshape(target, (-1, 1))
+learning_rate = 0.001
+
+x_data1 = tf.placeholder(tf.float32, [None, 10])
+x_data2 = tf.placeholder(tf.float32, [None, 10])
+y_data = tf.placeholder(tf.float32, [None, 1])
+
+# weights & bias for nn layers
+W1a = tf.Variable(tf.random_normal([10, 1]))
+b1a = tf.Variable(tf.random_normal([1]))
+L1a = tf.nn.sigmoid(tf.matmul(x_data1, W1a) + b1a)
+
+# W2a = tf.Variable(tf.random_normal([10, 1]))
+# b2a = tf.Variable(tf.random_normal([1]))
+# L2a = tf.nn.sigmoid(tf.matmul(L1a, W2a) + b2a)
+
+
+
+W1b = tf.Variable(tf.random_normal([10, 1]))
+b1b = tf.Variable(tf.random_normal([1]))
+L1b = tf.nn.sigmoid(tf.matmul(x_data2, W1b) + b1b)
+
+# W2b = tf.Variable(tf.random_normal([10, 1]))
+# b2b = tf.Variable(tf.random_normal([1]))
+# L2b = tf.nn.sigmoid(tf.matmul(L1b, W2b) + b2b)
+
+x_merged = tf.concat([L1a, L1b], 1)
+W3 = tf.Variable(tf.random_normal([2, 1]))
+b3 = tf.Variable(tf.random_normal([1]))
+
+hypothesis = tf.sigmoid(tf.matmul(x_merged, W3) + b3)
+
+# cost/loss function
+cost = -tf.reduce_mean(y_data * tf.log(hypothesis) + (1 - y_data) * tf.log(1 - hypothesis))
+
+train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+
+# Accuracy computation
+# True if hypothesis>0.5 else False
+predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, y_data), dtype=tf.float32))
+
+# Launch graph
+with tf.Session() as sess:
+    # Initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(20001):
+        cost_val, _ = sess.run([cost, train], feed_dict={x_data1: train1, x_data2: train2, y_data: target})
+        if step % 1000 == 0:
+            print(step, cost_val)
+
+    # Accuracy report
+    h, c, a = sess.run([hypothesis, predicted, accuracy],
+                       feed_dict={x_data1: train1, x_data2: train2, y_data: target})
+    print("\nHypothesis: ", h, "\nCorrect (Y): ", c, "\nAccuracy: ", a)
