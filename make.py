a3edec193fd1a6e132dafd02c474f032940ab0a0
songjein <jeinsong100@gmail.com>
2017-05-25 08:40:32 +0900

    new test dataset

--- make.py
+++ make.py
@@ -1,15 +1,15 @@
 import random
 
-wf1 = open("ts1.txt", "a")
-wf2 = open("ts2.txt", "a")
-wf3 = open("ts3.txt", "a")
+wf1 = open("ts11.txt", "a")
+wf2 = open("ts22.txt", "a")
+wf3 = open("ts33.txt", "a")
 
 
 def getRandList1(l):
 	return [str(random.randrange(1,20)) for i in range(l)]
 
 def getRandList2(l):
-	return [str(random.randrange(120,200)) for i in range(l)]
+	return [str(random.randrange(1,200)) for i in range(l)]
 
 for i in range(1000):
 	wf1.write(",".join(getRandList1(10)) + "\n")
