1feba350b4a89e22318258f94a2f6711f34ad53e
songjein <jeinsong100@gmail.com>
2017-05-29 20:16:33 +0900

    save and restore

--- word2vec/word2vec_basic.py
+++ word2vec/word2vec_basic.py
@@ -31,44 +31,44 @@ from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
 
 # Read the data into a list of strings.
-def read_data(filename):
-
-    data = []
-    with open(filename, 'r', encoding='utf-8') as f:
-        reader = csv.reader(f, delimiter=',')
-        # 첫줄 입력 건너 띄고, 물음표 스플릿 하는 부분 추가
-        for line in reader:
-            data += line[3].split() + line[4].split()
-    return data
+def make_voca(filename):
+	data = []
+	#with open(filename, 'r', encoding='utf-8') as f:
+	with open(filename, 'r') as f:
+		reader = csv.reader(f, delimiter=',')
+		for line in reader:
+			data += line[3].split() + line[4].split()
+	return data
 
 filename = './train.csv'
-vocabulary = read_data(filename)
+vocabulary = make_voca(filename)
 
 print('Data size', len(vocabulary))
 
 # # Step 2: Build the dictionary and replace rare words with UNK token.
 vocabulary_size = 50000
 
-
+# make data=>index, dictionary, reverse index....
 def build_dataset(words, n_words):
-  """Process raw inputs into a dataset."""
-  count = [['UNK', -1]]
-  count.extend(collections.Counter(words).most_common(n_words - 1))
-  dictionary = dict()
-  for word, _ in count:
-    dictionary[word] = len(dictionary)
-  data = list()
-  unk_count = 0
-  for word in words:
-    if word in dictionary:
-      index = dictionary[word]
-    else:
-      index = 0  # dictionary['UNK']
-      unk_count += 1
-    data.append(index)
-  count[0][1] = unk_count
-  reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
-  return data, count, dictionary, reversed_dictionary
+	"""Process raw inputs into a dataset."""
+	count = [['UNK', -1]]
+	count.extend(collections.Counter(words).most_common(n_words - 1))
+	dictionary = dict()
+	for word, _ in count:
+		dictionary[word] = len(dictionary)
+	data = list()
+	unk_count = 0
+	for word in words:
+		if word in dictionary:
+			index = dictionary[word]
+		else:
+			index = 0  # dictionary['UNK']
+			unk_count += 1
+		data.append(index)
+	count[0][1] = unk_count
+	reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+	# data: input data => index , count => each word count, dictionary => word:index, reversed_dictionary
+	return data, count, dictionary, reversed_dictionary
 
 data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
                                                             vocabulary_size)
@@ -77,7 +77,6 @@ print('Most common words (+UNK)', count[:5])
 print('Sample data', data[:10], [reverse_dictionary[i] for i in data[:10]])
 
 data_index = 0
-
 
 # Step 3: Function to generate a training batch for the skip-gram model.
 def generate_batch(batch_size, num_skips, skip_window):
@@ -127,93 +126,99 @@ valid_examples = np.random.choice(valid_window, valid_size, replace=False)
 num_sampled = 64    # Number of negative examples to sample.
 
 
-
 graph = tf.Graph()
 
 with graph.as_default():
 
-  # Input data.
-  train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
-  train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
-  valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+	# Input data.
+	train_inputs = tf.placeholder(tf.int32, shape=[batch_size])
+	train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+	valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
 
-  # Ops and variables pinned to the CPU because of missing GPU implementation
-  with tf.device('/gpu:0'):
-    # Look up embeddings for inputs.
-    embeddings = tf.Variable(
-        tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
-    embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+	# Ops and variables pinned to the CPU because of missing GPU implementation
+	with tf.device('/cpu:0'):
+		# Look up embeddings for inputs.
+		embeddings = tf.Variable(
+				tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0))
+		embed = tf.nn.embedding_lookup(embeddings, train_inputs)
 
-    # Construct the variables for the NCE loss
-    nce_weights = tf.Variable(
-        tf.truncated_normal([vocabulary_size, embedding_size],
-                            stddev=1.0 / math.sqrt(embedding_size)))
-    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+		# Construct the variables for the NCE loss
+		nce_weights = tf.Variable(
+				tf.truncated_normal([vocabulary_size, embedding_size],
+														stddev=1.0 / math.sqrt(embedding_size)))
+		nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
-  # Compute the average NCE loss for the batch.
-  # tf.nce_loss automatically draws a new sample of the negative labels each
-  # time we evaluate the loss.
-  loss = tf.reduce_mean(
-      tf.nn.nce_loss(weights=nce_weights,
-                     biases=nce_biases,
-                     labels=train_labels,
-                     inputs=embed,
-                     num_sampled=num_sampled,
-                     num_classes=vocabulary_size))
+	# Compute the average NCE loss for the batch.
+	# tf.nce_loss automatically draws a new sample of the negative labels each
+	# time we evaluate the loss.
+	loss = tf.reduce_mean(
+		tf.nn.nce_loss(weights=nce_weights,
+									 biases=nce_biases,
+									 labels=train_labels,
+									 inputs=embed,
+									 num_sampled=num_sampled,
+									 num_classes=vocabulary_size))
 
-  # Construct the SGD optimizer using a learning rate of 1.0.
-  optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
+	# Construct the SGD optimizer using a learning rate of 1.0.
+	optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(loss)
 
-  # Compute the cosine similarity between minibatch examples and all embeddings.
-  norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
-  normalized_embeddings = embeddings / norm
-  valid_embeddings = tf.nn.embedding_lookup(
-      normalized_embeddings, valid_dataset)
-  similarity = tf.matmul(
-      valid_embeddings, normalized_embeddings, transpose_b=True)
+	# Compute the cosine similarity between minibatch examples and all embeddings.
+	norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
+	normalized_embeddings = embeddings / norm
+	valid_embeddings = tf.nn.embedding_lookup(
+		normalized_embeddings, valid_dataset)
+	similarity = tf.matmul(
+		valid_embeddings, normalized_embeddings, transpose_b=True)
 
-  # Add variable initializer.
-  init = tf.global_variables_initializer()
+	# Add variable initializer.
+	init = tf.global_variables_initializer()
+
+	# Add ops to save and restore all the variables
+	saver = tf.train.Saver()
 
 # Step 5: Begin training.
 num_steps = 100001
 
 with tf.Session(graph=graph) as session:
-  # We must initialize all variables before we use them.
-  init.run()
-  print('Initialized')
+	# We must initialize all variables before we use them.
+	init.run()
+	print('Initialized')
 
-  average_loss = 0
-  for step in xrange(num_steps):
-    batch_inputs, batch_labels = generate_batch(
-        batch_size, num_skips, skip_window)
-    feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+	average_loss = 0
+	for step in xrange(num_steps):
+		batch_inputs, batch_labels = generate_batch(
+			batch_size, num_skips, skip_window)
+		feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
 
-    # We perform one update step by evaluating the optimizer op (including it
-    # in the list of returned values for session.run()
-    _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
-    average_loss += loss_val
+		# We perform one update step by evaluating the optimizer op (including it
+		# in the list of returned values for session.run()
+		_, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
+		average_loss += loss_val
 
-    if step % 2000 == 0:
-      if step > 0:
-        average_loss /= 2000
-      # The average loss is an estimate of the loss over the last 2000 batches.
-      print('Average loss at step ', step, ': ', average_loss)
-      average_loss = 0
+		if step % 2000 == 0:
+			if step > 0:
+				average_loss /= 2000
+			# The average loss is an estimate of the loss over the last 2000 batches.
+			print('Average loss at step ', step, ': ', average_loss)
+			average_loss = 0
 
-    # Note that this is expensive (~20% slowdown if computed every 500 steps)
-    if step % 10000 == 0:
-      sim = similarity.eval()
-      for i in xrange(valid_size):
-        valid_word = reverse_dictionary[valid_examples[i]]
-        top_k = 8  # number of nearest neighbors
-        nearest = (-sim[i, :]).argsort()[1:top_k + 1]
-        log_str = 'Nearest to %s:' % valid_word
-        for k in xrange(top_k):
-          close_word = reverse_dictionary[nearest[k]]
-          log_str = '%s %s,' % (log_str, close_word)
-        print(log_str)
-  final_embeddings = normalized_embeddings.eval()
+		# Note that this is expensive (~20% slowdown if computed every 500 steps)
+		if step % 10000 == 0:
+			sim = similarity.eval()
+			for i in xrange(valid_size):
+				valid_word = reverse_dictionary[valid_examples[i]]
+				top_k = 8  # number of nearest neighbors
+				nearest = (-sim[i, :]).argsort()[1:top_k + 1]
+				log_str = 'Nearest to %s:' % valid_word
+				for k in xrange(top_k):
+					close_word = reverse_dictionary[nearest[k]]
+					log_str = '%s %s,' % (log_str, close_word)
+				print(log_str)
+	final_embeddings = normalized_embeddings.eval()
+
+	# save the variables to disk
+	save_path = saver.save(session, "./saver/w2v.ckpt")
+	print ("model saved in file: %s" % save_path)
 
 # Step 6: Visualize the embeddings.
 
